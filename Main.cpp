0d66b36c515ee7ef983943e9c79d2175dcd77597
aidan96-code <aidancochrane123@gmail.com>
2026-09-11 16:11:36 +0100

    added sister class

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,8 @@
-//t
-
 #include <iostream>
 #include <string>
 using namespace std;
+
+//spacemarine class
 
 class spacemarine {
 public:
@@ -16,11 +16,30 @@ public:
 	}
 };
 
+//sister class
+
+class SoB {
+public:
+	string rank;
+	string name;
+	int wounds;
+	SoB(string x, string y, int z) {
+		rank = x;
+		name = y;
+		wounds = z;
+	}
+};
+
+//main function
+
 int main() {
 	spacemarine Titus("leader", "captain", 500);
 	spacemarine Angelos("leader", "chaptermaster", 1000);
+	SoB Celestine("Saint", "Celestine", 8);
+	SoB Maria("battle sister", "Maria", 1);
 
 	cout << Titus.role << " " << Titus.rank << " " << Titus.age << "\n";
 	cout << Angelos.role << " " << Angelos.rank << " " << Angelos.age << "\n";
+	cout << Celestine.rank << " " << Celestine.name << " " << Celestine.wounds << "\n";
 }
 
