6aea153232834b8928349210dfc3cde027b9467c
aidan96-code <aidancochrane123@gmail.com>
2026-09-11 16:00:28 +0100

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class spacemarine {
+public:
+	string role;
+	string rank;
+	int age;
+	spacemarine(string x, string y, int z) {
+		role = x;
+		rank = y;
+		age = z;
+	}
+};
+
+int main() {
+	spacemarine Titus("leader", "captain", 500);
+	spacemarine Angelos("leader", "chaptermaster", 1000);
+
+	cout << Titus.role << " " << Titus.rank << " " << Titus.age << "\n";
+	cout << Angelos.role << " " << Angelos.rank << " " << Angelos.age << "\n";
+}
+
